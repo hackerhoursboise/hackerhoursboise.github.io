427d560818d164df4030972e711d7f6a2f86e6d0
Craig Kofoed <Craig@Craigs-MacBook-Pro.local>
2015-12-29 20:24:40 -0700

    testing countdown function

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function getTime(){
- $.getJSON('https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=hackerhoursboise&only=time&photo-host=public&page=1&fields=&order=time&desc=false&status=upcoming&sig_id=4397595&sig=d0f2e99cd0a29d2587e937191612dcc0c903c5ba&callback=?', function(data){
-    var dataArray = data.results[0];
+ var apiTime = $.getJSON('https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=hackerhoursboise&only=time&photo-host=public&page=1&fields=&order=time&desc=false&status=upcoming&sig_id=4397595&sig=d0f2e99cd0a29d2587e937191612dcc0c903c5ba&callback=?') 
+
+ 	apiTime.done(countDown).fail(error);
+   
+};
+
+
+function countDown(apiTime){
+	 
+    var dataArray = apiTime.results[0];
     var time;
     $.each(dataArray, function(key, value){
       //items.push( "<li id='" + key + "'>" + value + "</li>" );
@@ -11,15 +19,8 @@ function getTime(){
     var d = new Date(time); 
     //alert(d.toString());
     
-
     $(".content").append('<p> next meeting on '+ d.toString() +'</p>');
-   
-  })
-   countDown(time);
-}
-
-
-function countDown(time){
+  
 	var nextMeetUp = time,
 	    seconds = Math.floor((nextMeetup - new Date().getTime()) / 1000),
 	    minutes = Math.floor(seconds / 60),
