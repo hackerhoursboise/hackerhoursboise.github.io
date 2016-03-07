42535fa34e17480362b1c8e4ea72581a57c27215
Lindsey <lindseydwhitney@gmail.com>
2016-03-07 15:09:34 -0700

    Added responsive design to home, about, resources, and projects sections

--- script.js
+++ script.js
@@ -66,3 +66,20 @@ function handleErr(jqxhr, textStatus, err) {
   console.log("Request Failed: " + jqxhr.status + textStatus + ", " + err);
   console.debug(jqxhr);
 }
+
+
+// Smooth Scrolling on Main Navigation ==============================
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
